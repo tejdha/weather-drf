6acf8d6992e6dba576bfae4297b05b266e5683ed
tejprabha <dtejaa0226@gmail.com>
2026-05-24 16:53:42 +0530

    deployment setup

--- bookstracker/settings.py
+++ bookstracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rm_bnos))p!!e6$p0lyf75*7+y2@hu1lw@hjko%%*331cb^!97
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
